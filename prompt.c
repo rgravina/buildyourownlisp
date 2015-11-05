38d3d80bc145ad6233a4c5e1f2e4fc76f0a8d89d
Robert Gravina <robert.gravina@kii.com>
2015-11-05 15:25:48 +0900

    Use gnu readline to get input.

--- prompt.c
+++ prompt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-
-// user input buffer
-static char input[2048];
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main(int argc, char** argv) {
   // version info
@@ -11,12 +11,14 @@ int main(int argc, char** argv) {
 
   // REPL loop
   while (true) {
-    // output prompt
-    fputs("lispy> ", stdout);
-    // read a line of user input into the buffer
-    fgets(input, 2048, stdin);
+    // output prompt and read user input
+    char* input = readline("lispy> ");
+    // add input to history
+    add_history(input);
     // for now, just echo the input back
-    printf("You entered: %s", input);
+    printf("You entered: %s\n", input);
+    // free retrieved input
+    free(input);
   }
 
   return 0;
