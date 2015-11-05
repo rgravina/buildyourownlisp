38b6dea615c62617b7d56a163bbafe079db882ef
Robert Gravina <robert.gravina@kii.com>
2015-11-05 17:23:53 +0900

    Add Windows support. Changed prompt.

--- prompt.c
+++ prompt.c
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+// readline isn't available on Windows so provide a similar interface.
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+// add_history here just does nothing
+void add_history(char* unused) {}
+#else
 #include <readline/readline.h>
 #include <readline/history.h>
+#endif
 
 int main(int argc, char** argv) {
   // version info
-  puts("Lispy Version 0.1");
+  puts("Build Your Own Lip: Version 0.1");
   puts("Press Ctrl+c to Exit\n");
 
   // REPL loop
   while (true) {
     // output prompt and read user input
-    char* input = readline("lispy> ");
+    char* input = readline("lisp> ");
     // add input to history
     add_history(input);
     // for now, just echo the input back
